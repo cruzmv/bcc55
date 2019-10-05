11ce5576871352a0414fc68332eee1cd1b1d9554
Mauricio <=>
2019-10-05 23:30:00 +0100

    First commit

---
+++ Bin/path_xharbour_bcc.bat
@@ -0,0 +1,7 @@
+SET PATH=%PATH%;D:\DEVEL\BCC55\BIN;D:\DEVEL\BCC55\LIB;D:\DEVEL\BCC55\INCLUDE;D:\DEVEL\xharbour_maio_2013;D:\DEVEL\xharbour_maio_2013\bin;D:\DEVEL\xharbour_maio_2013\lib;D:\DEVEL\xharbour_maio_2013\include
+SET INCLUDE=%INCLUDE%;D:\DEVEL\Bcc55\include;D:\DEVEL\xharbour_maio_2013\include;D:\Devel\xharbour_maio_2013\contrib\hwgui\include
+SET LIB=%LIB%;D:\DEVEL\Bcc55\lib;D:\DEVEL\xharbour_maio_2013\lib
+SET HB_PATH=D:\DEVEL\xharbour_maio_2013
+SET QUERYDEBUG=Y
+SET QUERYDEBUGCOUNTER=Y
+%SystemRoot%\system32\cmd.exe
